f08a8c0081f86f3fddd28d503ef805411ca746cf
Manuel Leduc <manuel.leduc@inria.fr>
2015-11-06 16:59:41 +0100

    First version

---
+++ TwitterClient.cs
@@ -0,0 +1,84 @@
+﻿using Org.Kevoree.Annotation;
+using Org.Kevoree.Core.Api;
+using Org.Kevoree.Log.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Org.Kevoree.Library.Comp.Twitter.Client
+{
+    [ComponentType]
+    [Serializable]
+    public class TwitterClient
+    {
+        [KevoreeInject]
+        private ILogger _logger;
+
+        [Output]
+        private Port twit;
+
+        [Param(Optional = false)]
+        private string consumerKey;
+
+        [Param(Optional = false)]
+        private string consumerSecret;
+
+        [Param(Optional = false)]
+        private string accessToken;
+
+        [Param(Optional = false)]
+        private string accessTokenSecret;
+
+        [Param(Optional = false)]
+        private string keyword;
+
+        private TwitterClientThread tcInstance;
+
+        [Start]
+        public void Start()
+        {
+            tcInstance = new TwitterClientThread(this);
+            new Thread(new ThreadStart(tcInstance.Start)).Start();
+        }
+
+        [Stop]
+        public void Stop()
+        {
+            this.tcInstance.Stop();
+        }
+
+        internal string getConsumerKey()
+        {
+            return this.consumerKey;
+        }
+
+        internal string getConsumerSecret()
+        {
+            return this.consumerSecret;
+        }
+
+        internal string getKeyword()
+        {
+            return this.keyword;
+        }
+
+        internal Port getTwit()
+        {
+            return this.twit;
+        }
+
+        internal string getAccessToken()
+        {
+            return this.accessToken;
+        }
+
+        internal string getAccessTokenSecret()
+        {
+            return this.accessTokenSecret;
+        }
+    }
+}
---
+++ TwitterClientThread.cs
@@ -0,0 +1,80 @@
+﻿using LinqToTwitter;
+using Newtonsoft.Json.Linq;
+using Org.Kevoree.Core.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Org.Kevoree.Library.Comp.Twitter.Client
+{
+    class TwitterClientThread
+    {
+        private TwitterClient twitterClient;
+
+        private bool _stop = false;
+
+        public TwitterClientThread(TwitterClient twitterClient)
+        {
+            this.twitterClient = twitterClient;
+        }
+        internal void Start()
+        {
+            var auth = new SingleUserAuthorizer
+            {
+                CredentialStore = new SingleUserInMemoryCredentialStore
+                {
+                    ConsumerKey = twitterClient.getConsumerKey(),
+                    ConsumerSecret = twitterClient.getConsumerSecret(),
+                    AccessToken = twitterClient.getAccessToken(),
+                    AccessTokenSecret = twitterClient.getAccessTokenSecret()
+                }
+            };
+
+            var twitterCtx = new TwitterContext(auth);
+
+            callAsync(twitterCtx, twitterClient.getKeyword(), twitterClient.getTwit());
+
+            while (!_stop)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        private async static void callAsync(TwitterContext twitterCtx, string keyword, Port port)
+        {
+            var cancelTokenSrc = new CancellationTokenSource();
+
+            await
+                    (from strm in twitterCtx.Streaming.WithCancellation(cancelTokenSrc.Token)
+                     where strm.Type == StreamingType.Filter && strm.Track == keyword
+                     select strm).StartAsync(async strm => { await HandleStreamResponse(strm, port); });
+        }
+
+        static async Task<int> HandleStreamResponse(StreamContent strm, Port port)
+        {
+            switch (strm.EntityType)
+            {
+                case StreamEntityType.Status:
+                    var status = strm.Entity as Status;
+                    JObject o = new JObject();
+                    o["Account"] = status.User.ScreenNameResponse;
+                    o["Message"] = status.Text;
+                    port.send(o.ToString(), null);
+                    break;
+                default:
+                    // we only handle Status messages, other are just skipped.
+                    break;
+            }
+
+            return await Task.FromResult(0);
+        }
+
+        internal void Stop()
+        {
+            this._stop = true;
+        }
+    }
+}
